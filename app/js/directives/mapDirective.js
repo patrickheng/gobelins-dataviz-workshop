58c91e02ca393f8d3cdd3093641f25f87864a258
Patrick HENG <hengpatrick.pro@gmail.com>
2015-11-23 22:28:28 +0100

    Testing map

---
+++ app/js/directives/mapDirective.js
@@ -0,0 +1,30 @@
+'use strict';
+import 'gsap';
+
+/**
+ * @ngdoc directive
+ * @name map
+ * @requires
+ * @module app
+ * @restrict E
+ */
+function map($rootScope) {
+
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/map.html',
+    replace: true,
+
+    link: (scope, element) => {
+      const mapGround = element[0].querySelectorAll('.map-below .map-ground');
+      const tl = new TimelineMax();
+
+      tl.staggerFromTo(mapGround, 0.5, {scale:1.5, opacity:0}, { scale: 1, opacity: 1, ease: Cubic.easeOut}, 0.008);
+    }
+  };
+}
+
+export default {
+  name: 'map',
+  fn: map
+};
